17b041a5bba02d2c1c21c1d50ae683d1f6a2c4a3
Violet <Pyroan@users.noreply.github.com>
2023-12-21 13:50:04 -0800

    add fancy highlighting to board graphics

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
-'''fuck everyone who's written their own solver i'm writing mine because i'm special
-and my wheels are best'''
+'''i'm special and my wheels are best'''
 import argparse
 import json
 import re
 from collections import Counter
+
+from colorama import Fore, Style
+
+from board import Board
 
 with open('config.json') as f:
     config = json.load(f)
@@ -34,54 +37,16 @@ def permutations(k: int, a: list) -> list:
             i += 1
 
 
-def analyzeRows(board: list[int]):
+def generate_possible_boards(board: str) -> list[Board]:
     '''
-    return an array of the sum of each 'row' in the cactpot board
-    '''
-    rowsums = [0]*8
-    # LR diagonal
-    rowsums[0] = sum(board[0::4])
-    # Columns
-    for i in range(3):
-        rowsums[i+1] = sum(board[i::3])
-    # RL Diagonal
-    rowsums[4] = sum(board[2:7:2])
-    # Rows
-    for i in range(3):
-        rowsums[i+5] = sum(board[3*i:3*(i+1)])
-    return rowsums
-
-
-def analyze_possible_boards(board: str):
-    '''
-    compute the possible payouts for each row for all possible boards given
-    the known board information
+    generate all possible boards for the given set of known values
     '''
     missingno = list(set('123456789')-set(board))
-    row_results = []
+    boards = []
     for i in permutations(len(missingno), missingno):
-        newboard = board.replace('?', '{}').format(*i)
-        newboard = [*map(int, list(newboard))]
-        row_results.append(analyzeRows(newboard))
-    return row_results
-
-
-def printboard(board: list, rowsums: list[int]):
-    vals = (*rowsums[:6], *board[:3], rowsums[6],
-            *board[3:6], rowsums[7], *board[6:])
-    print(
-        '''\
-  {:5}  {:^5} {:^5} {:^5}  {:<5}  
-      ↘    ↓     ↓     ↓    ↙
-        ┌─────┬─────┬─────┐
-{:>5} → │  {}  │  {}  │  {}  │
-        ├─────┼─────┼─────┤
-{:>5} → │  {}  │  {}  │  {}  │
-        ├─────┼─────┼─────┤
-{:>5} → │  {}  │  {}  │  {}  │
-        └─────┴─────┴─────┘
-    '''.format(*vals)
-    )
+        newboard = Board(board.replace('?', '{}').format(*i))
+        boards.append(newboard)
+    return boards
 
 
 if __name__ == "__main__":
@@ -90,20 +55,19 @@ if __name__ == "__main__":
         'board', type=str, help="A 9-character string representing a cactpot board. Values ordered lexicographically. Replace unknown values with `?` e.g. '12??5???9'")
 
     args = parser.parse_args()
-    board = args.board
+    board_str = args.board
 
     # board validation for some reason
-    board_without_unknowns = [*filter(lambda x: x != '?', board)]
-    if not re.fullmatch(r'[0-9\?]{9}', board) or len(set(board_without_unknowns)) != len(board_without_unknowns):
+    board_str_without_unknowns = [*filter(lambda x: x != '?', board_str)]
+    if not re.fullmatch(r'[0-9\?]{9}', board_str) or len(set(board_str_without_unknowns)) != len(board_str_without_unknowns):
         raise ValueError("Invalid cactpot board!")
 
-    row_results = analyze_possible_boards(board)
-    row_averages = [sum(payouts[str(r[i])] for r in row_results) // len(row_results)
-                    for i in range(len(row_results[0]))]
-    row_medians = [sorted(payouts[str(r[i])] for r in row_results)[
-        len(row_results)//2] for i in range(len(row_results[0]))]
-    # print(Counter(payouts[str(r[3])] for r in row_results))
+    possible_boards = generate_possible_boards(board_str)
+    row_averages = [sum(payouts[str(b.analyze_rows()[i])] for b in possible_boards) // len(possible_boards)
+                    for i in range(8)]
     print("Average Payouts:")
-    printboard(board, row_averages)
-    print("Median Payouts:")
-    printboard(board, row_medians)
+    board = Board(board_str)
+    for i in range(len(row_averages)):
+        if row_averages[i] == max(row_averages):
+            board.highlight_row(i, Style.BRIGHT+Fore.GREEN)
+    board.draw(row_averages)
---
+++ board.py
@@ -0,0 +1,68 @@
+from colorama import Fore, Style, just_fix_windows_console
+
+just_fix_windows_console()
+
+
+class Board:
+    '''hundo p this only exists because of all the fancy highlighting i'd like to do'''
+
+    def __init__(self, values: str):
+        self.values = values
+        self.cell_highlights = [None]*len(self.values)
+        for i in range(len(self.values)):
+            if self.values[i] == '?':
+                self.cell_highlights[i] = Style.BRIGHT + Fore.BLACK
+
+    def analyze_rows(self):
+        values = [*map(int, list(self.values))]
+        row_sums = [0]*8
+        # LR diagonal
+        row_sums[0] = sum(values[0::4])
+        # Columns
+        for i in range(3):
+            row_sums[i+1] = sum(values[i::3])
+        # RL Diagonal
+        row_sums[4] = sum(values[2:7:2])
+        # Rows
+        for i in range(3):
+            row_sums[i+5] = sum(values[3*i:3*(i+1)])
+        return row_sums
+
+    def highlight_row(self, row_id: int, style: str):
+        if row_id == 0:
+            for i in [0, 4, 8]:
+                self.cell_highlights[i] = style
+        elif 1 <= row_id <= 3:
+            j = row_id-1
+            for i in [j, j+3, j+6]:
+                self.cell_highlights[i] = style
+        elif row_id == 4:
+            for i in [2, 4, 6]:
+                self.cell_highlights[i] = style
+        elif 5 <= row_id <= 7:
+            j = (row_id-5)*3
+            for i in [j, j+1, j+2]:
+                self.cell_highlights[i] = style
+
+    def draw(self, row_captions=None):
+        cell_values = list(self.values)
+        # Apply highlights
+        for i in range(len(cell_values)):
+            if self.cell_highlights[i] != None:
+                cell_values[i] = f'{self.cell_highlights[i]}{cell_values[i]}{Style.RESET_ALL}'
+        # Preformat values to be inserted
+        vals = (*row_captions[:6], *cell_values[:3], row_captions[6],
+                *cell_values[3:6], row_captions[7], *cell_values[6:])
+        print(
+            '''\
+  {:5}  {:^5} {:^5} {:^5}  {:<5}  
+      ↘    ↓     ↓     ↓    ↙
+        ┌─────┬─────┬─────┐
+{:>5} → │  {}  │  {}  │  {}  │
+        ├─────┼─────┼─────┤
+{:>5} → │  {}  │  {}  │  {}  │
+        ├─────┼─────┼─────┤
+{:>5} → │  {}  │  {}  │  {}  │
+        └─────┴─────┴─────┘
+        '''.format(*vals)
+        )
